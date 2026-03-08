c1cc80582a9b0937a8529abde9fca783ac423c51
khaledessam2 <khaledessam3000@gmail.com>
2026-03-08 13:22:42 +0200

    create toggle side menu

--- src/app/services/layout.service.ts
+++ src/app/services/layout.service.ts
@@ -3,6 +3,7 @@ import { Injectable, signal } from '@angular/core';
 @Injectable({ providedIn: 'root' })
 export class LayoutService {
   sidebarOpen = signal(false);
+  sidebarCollapsed = signal(false);
 
   toggle(): void {
     this.sidebarOpen.update(v => !v);
@@ -10,5 +11,9 @@ export class LayoutService {
 
   close(): void {
     this.sidebarOpen.set(false);
+  }
+
+  toggleCollapse(): void {
+    this.sidebarCollapsed.update(v => !v);
   }
 }
