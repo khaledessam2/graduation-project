64b99cc86af2bc6161f06c449e8d6dfe78c678f6
khaledessam2 <khaledessam3000@gmail.com>
2026-04-08 14:11:52 +0200

    loading

--- src/app/interceptors/api.interceptor.ts
+++ src/app/interceptors/api.interceptor.ts
@@ -1,8 +1,15 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { finalize } from 'rxjs';
+import { LoadingService } from '../services/loading.service';
 
 export const apiInterceptor: HttpInterceptorFn = (req, next) => {
+  const loading = inject(LoadingService);
+  loading.start();
+
   const apiReq = req.clone({
     setHeaders: { 'Content-Type': 'application/json' },
   });
-  return next(apiReq);
+
+  return next(apiReq).pipe(finalize(() => loading.stop()));
 };
